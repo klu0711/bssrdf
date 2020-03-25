767cb06263837c18f943610fe8c45553158312ab
Kristoffer Lundgren <klu0711@gmail.com>
2020-03-25 15:59:54 +0100

    Removed redundant code

--- PathTracing/material.h
+++ PathTracing/material.h
@@ -92,7 +92,7 @@ public:
     {
 
 
-        if(drand48() < 0.1 && rec.insideMat)
+        if(rec.insideMat)
         {
             ray scattered = ray(rec.p, randomInUnitSphere());
             sRec.attenuation = albedo->value(rec.u, rec.v, rec.p);
@@ -149,52 +149,7 @@ public:
     }
     texture* albedo;
 };
-/*
-class dielectric : public material
-{
-public:
-    dielectric(float ri) : refIdx(ri) {}
-    virtual bool scatter(const ray& rIn, const hitRecord& rec, Vector4D& attenuation, ray& scattered) const
-    {
-        Vector4D outwardNormal;
-        Vector4D reflected = reflect(rIn.direction(), rec.normal);
-        float niOverNt;
-        attenuation = Vector4D(1.0, 1.0, 1.0, 1);
-        Vector4D refracted;
-        float reflectProb;
-        float cosine;
-        if(rIn.direction().dotProduct(rec.normal) > 0)
-        {
-            outwardNormal = rec.normal * -1;
-            niOverNt = refIdx;
-            cosine = refIdx * rIn.direction().dotProduct(rec.normal) / rIn.direction().length();
-        }else
-        {
-            outwardNormal = rec.normal;
-            niOverNt = 1.0 / refIdx;
-            cosine = (rIn.direction().dotProduct(rec.normal)*-1)/rIn.direction().length();
-        }if(refract(rIn.direction(), outwardNormal, niOverNt, refracted))
-        {
-            reflectProb = schlick(cosine, refIdx);
 
-        }else
-        {
-            scattered = ray(rec.p, reflected);
-            reflectProb = 1.0;
-        }if(drand48() < reflectProb)
-        {
-            scattered = ray(rec.p, reflected);
-        }else
-        {
-            scattered = ray(rec.p, refracted);
-        }
-        return true;
-
-    }
-
-    float refIdx;
-};
-*/
 
 class metal : public material
 {
